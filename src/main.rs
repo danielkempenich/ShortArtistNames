8997e0b8a600f01c102acb5dd15f51680d840896
danielkempenich <dan.kempenich@gmail.com>
2023-09-21 15:52:02 -0500

    Play around with library and get used to rust a bit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use id3::{Error, ErrorKind, Tag, TagLike, Frame};
+
+// Using TSO2 - AlbumArtistSortOrder, for our purposes here
+const SHORT_ARTIST_TAG_NAME: &str = "TSO2";
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut tag = match Tag::read_from_path("C:\\temp\\test_file.mp3") {
+        Ok(tag) => tag,
+        Err(Error{kind: ErrorKind::NoTag, ..}) => Tag::new(),
+        Err(..) => return (),
+    };
+    
+    let artist_long_name:&str;
+    if let Some(album_artist) = tag.album_artist() {
+        println!("album_artist: {}", album_artist);
+        artist_long_name = album_artist;
+    }
+    else if let Some(album_artist) = tag.artist() {
+        println!("artist: {}", album_artist);
+        artist_long_name = album_artist;
+    }
+    else {    
+        println!("no album_artist");
+        return ();
+    }
+
+    if let Some(short_artist) = tag.get(SHORT_ARTIST_TAG_NAME) {
+        println!("short_artist: {}", short_artist);
+    }
+    else {
+        println!("Setting \"Short artist name\" = {}", artist_long_name.to_string());
+        tag.add_frame(Frame::text(SHORT_ARTIST_TAG_NAME, artist_long_name.to_string()));
+        println!("added short artist");
+        let _ = tag.write_to_path("C:\\temp\\test_file.mp3", id3::Version::Id3v24);
+    }
 }
